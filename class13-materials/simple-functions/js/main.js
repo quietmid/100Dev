4ff4bf97498fc983cb528c8b9c643b92ed9a7e89
quietmid <ninjonglu@gmail.com>
2023-03-09 15:52:15 +0200

    function

---
+++ class13-materials/simple-functions/js/main.js
@@ -0,0 +1,23 @@
+//create a function that adds two numbers and alerts the sum
+function add(a,b){
+    let sum = a + b;
+    alert(sum)
+}
+
+add(2,5)
+//create a function that multiplys three numbers and console logs the product
+
+function multiply(a1,b2,c3) {
+    let sum = a1 * b2 * c3;
+    console.log(sum)
+}
+
+multiply(5, 6, 7)
+//create a function that divides two numbers and returns the ???
+
+function divides (num1, num2) {
+    let sum = num1/num2;
+    return sum
+}
+
+console.log(divides(10, 5))
